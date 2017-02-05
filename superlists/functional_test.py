5d76386bc1bb54a224b1be98091a4862df96657f
suhyun kim <ideptor@gmail.com>
2017-02-06 01:13:39 +0900

    CH4 p44 modify functional test to check whether task is appended

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -10,10 +11,29 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrive_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn("To-Do", header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
+        )
+
+        inputbox.send_keys('공작깃털 사기')
+
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(rows.text == '1: 공작깃털사기' for row in rows)
+        )
+
         self.fail("Finish the test!")
 
 
