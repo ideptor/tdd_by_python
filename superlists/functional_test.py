fe00baa5c4432a0f312b34fc4a6e78d610be6f52
suhyun kim <ideptor@gmail.com>
2017-02-06 01:34:02 +0900

    CH4 p53 write main HTML page with templates

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -31,7 +31,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(rows.text == '1: 공작깃털사기' for row in rows)
+            any(rows.text == '1: 공작깃털사기' for row in rows),
+            "신규 작업이 테이블에 표시되지 않는다"
         )
 
         self.fail("Finish the test!")
